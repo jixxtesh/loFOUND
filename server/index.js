46386ddd1890b04521222568b9ffef51910af148
jixxtesh <jixxtesh@gmail.com>
2025-09-26 15:18:43 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.enable("trust proxy");
 dotenv.config();
 connectDatabase();
-const allowedOrigins = ['http://localhost:3000', 'https://lofound-jixx.vercel.app/login'];
+const allowedOrigins = ['http://localhost:3000', 'https://lofound-jixx.vercel.app/'];
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
